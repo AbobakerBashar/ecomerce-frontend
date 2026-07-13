88115b6848f5929321bdcd030aaec0068a79e972
Abobaker Yagoub Bashar <bakorybashar@gmail.com>
2026-07-13 10:45:29 +0200

    feat: implement single product page

--- app/(public)/collections/[slug]/page.tsx
+++ app/(public)/collections/[slug]/page.tsx
@@ -1,23 +1,70 @@
-// import { products } from "@/lib/data";
-// import { notFound } from "next/navigation";
+import { notFound } from "next/navigation";
 
-// import SingleProductPage from "@/components/collections/SingleProductPage";
+import SingleProductPage from "@/components/collections/SingleProductPage";
+import { getProductBySlug } from "@/lib/product";
+import { FullProduct } from "@/types/product";
+import type { Metadata } from "next";
+import SimilarProducts from "@/components/collections/SimilarProducts";
 
-// export default async function ProductRoute({
-// 	params,
-// }: {
-// 	params: Promise<{ slug: string }>;
-// }) {
-// 	const { slug } = await params;
-
-// 	const product = products.find((p) => p.slug === slug);
-// 	if (!product) notFound();
-
-// 	return <SingleProductPage product={product} />;
-// }
-
-const page = () => {
-	return <div>page</div>;
+type Props = {
+	params: Promise<{
+		slug: string;
+	}>;
 };
 
-export default page;
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+	const { slug } = await params;
+
+	const product = await getProduct(slug);
+
+	if (!product) notFound();
+
+	return {
+		title: `${product.name}`,
+		description: product.description,
+
+		openGraph: {
+			title: product.name,
+			description: product.description,
+			images: [
+				{
+					url: product.images[0],
+					width: 800,
+					height: 800,
+					alt: product.name,
+				},
+			],
+		},
+
+		twitter: {
+			card: "summary_large_image",
+			title: product.name,
+			description: product.description,
+			images: [product.images[0]],
+		},
+	};
+}
+
+const getProduct = async (slug: string): Promise<FullProduct | null> => {
+	try {
+		const res = await getProductBySlug(slug);
+		return res.product;
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
+};
+
+export default async function ProductRoute({ params }: Props) {
+	const { slug } = await params;
+
+	const product = await getProduct(slug);
+
+	if (!product) notFound();
+
+	return (
+		<SingleProductPage product={product}>
+			<SimilarProducts categoryId={product.category.id} />
+		</SingleProductPage>
+	);
+}
